7dadefcedf745c0abd31ca2cd5d3d980cf17c458
JorgeRondon <31427929+JorgeRondon@users.noreply.github.com>
2017-09-09 18:34:42 -0500

    Add files via upload

---
+++ HW/RTL/05MicroSD/Version_01/02 verilog/modulo1.v
@@ -0,0 +1,18 @@
+module counter
+(
+	input             clkMaster,
+	input      [15:0]  DataIN,
+	input      [7:0]  Adress,
+	input     Enable,
+	input     R/W,
+	input     Done,
+	output     [15:0]  DataOUT
+
+	input     clkSlave,
+	input     MISO,
+	output     MOSI,	
+	output     CS,
+
+);
+
+endmodule
