1a720524a970cf731d9c65eb4a785697ec77b2c1
dfnietoa <31425841+dfnietoa@users.noreply.github.com>
2017-09-10 12:27:59 -0500

    Add files via upload

---
+++ HW/RTL/05MicroSD/Version_02/02 verilog/proyecto.v
@@ -0,0 +1,21 @@
+module proyecto
+	( 
+	input [15:0] dataIN,
+	input EN,
+	input reset,
+	input CLK
+	input DATA0,
+	input RW,
+	input [7:0] addres,
+	output [15:0] dataOUT,
+	output DONE,
+	output CMD,
+	output DATA3,
+	);
+
+
+
+
+
+
+endmodule
