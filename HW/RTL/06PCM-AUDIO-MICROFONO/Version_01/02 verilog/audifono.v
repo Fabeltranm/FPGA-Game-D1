cf490da1ffb00e7d9c97b54c1226baf7e12dbaa5
Lisseth Tatiana Herrera  Rosero <ltherreraro@unal.edu.co>
2017-09-12 23:22:50 -0500

    Create audifono.v

---
+++ HW/RTL/06PCM-AUDIO-MICROFONO/Version_01/02 verilog/audifono.v
@@ -0,0 +1,13 @@
+module microfono
+(
+	input     bclk,
+	output    [7:0]  Adress,
+	input     sdmode,
+	input     Reset,
+	input     [14:0]  Datain,
+	input     select,
+	output    done,	
+
+);
+
+endmodule
