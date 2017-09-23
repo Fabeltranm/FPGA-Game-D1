b48de36f7cfa48886b7049d02cf53f796ea1155b
AlexandraRV <31471883+AlexandraRV@users.noreply.github.com>
2017-09-23 10:25:44 -0500

    Update Codigo verilog.v

--- HW/RTL/06PCM-AUDIO-MICROFONO/Version_02/02 verilog/Codigo verilog.v
+++ HW/RTL/06PCM-AUDIO-MICROFONO/Version_02/02 verilog/Codigo verilog.v
@@ -1,13 +1,17 @@
 // Declaracion de variables de entrada al sistema
 
 module audio
-
-input Bclk // reloj de SHP0645M4H
-input ws
-input Din // dato de entrada
+  (
+input             CLK // reloj de FPGA
+input             ENABLE
+input             D_IN // dato de entrada
 
 // Declaracion de variables de salida del sistema
 
-output Bclk // reloj de MAX98357A
-output LRclk
-output Dout // dato de salida
+output             DONE // reloj de MAX98357A
+output             WS
+output             BCLK // reloj de SHP0645M4
+    output  [17:0] D_OUT // dato de salida
+  );
+  
+endmodule
