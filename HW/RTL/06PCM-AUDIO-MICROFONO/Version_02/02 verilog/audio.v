f51261bed4ac422b84e058528af86ee7c1064227
David Stevent Avila Vela <31441648+davidavila18@users.noreply.github.com>
2017-09-24 09:20:53 -0500

    Add files via upload

---
+++ HW/RTL/06PCM-AUDIO-MICROFONO/Version_02/02 verilog/audio.v
@@ -0,0 +1,11 @@
+module audio
+(
+	input             CLK,
+	input      [15:0] D_IN,
+	output            BCLK,
+	output            LRCLK,
+	output            DONE,
+	output     	  D_OUT,
+
+);
+endmodule
