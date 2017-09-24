c288e4b5e37036d12320e8166c9de10788e5fd3b
Daniel Alejandro Rodríguez Chávez <31452436+Dantiteis@users.noreply.github.com>
2017-09-24 09:06:18 -0500

    Add files via upload

---
+++ HW/RTL/08ULTRASONIDO/Version_02/02 verilog/contador.v
@@ -0,0 +1,12 @@
+module contador		(
+				
+				output	[0:7] count,
+				output	trigg,
+				output	doneC
+				input	ECHO,
+				input	ENABLE
+				
+			);
+
+
+endmodule
---
+++ HW/RTL/08ULTRASONIDO/Version_02/02 verilog/multiplo.v
@@ -0,0 +1,7 @@
+module	multiplo	(
+				input   [0:7]   count,
+				input		enableM,
+				output		DONE,
+				output	[0:7]	d
+			);
+endmodule
