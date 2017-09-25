88856f7b54997c32d42c10f7d635f7177f5c42af
jdocampom <jd.ocampom0117@icloud.com>
2017-09-24 21:32:28 -0500

    Add files via upload

---
+++ HW/RTL/04LVDS/Version_01/02 verilog/LVDS.v
@@ -0,0 +1,12 @@
+module LVDS(CLK, Image, DataEnable, Channel0, Channel1, Channel2)
+	( 
+	input CLK,
+	input Image,
+	input DataEnable,
+	output CLK,	
+	output Channel0,
+	output Channel1,
+	output Channel2,
+	);
+	end
+endmodule
