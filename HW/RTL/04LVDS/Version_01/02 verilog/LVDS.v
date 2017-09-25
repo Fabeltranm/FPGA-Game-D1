762266d19c55c77b11360f1ae2fe3846199d1e04
jdocampom <jd.ocampom0117@icloud.com>
2017-09-24 23:02:49 -0500

    Update LVDS.v

--- HW/RTL/04LVDS/Version_01/02 verilog/LVDS.v
+++ HW/RTL/04LVDS/Version_01/02 verilog/LVDS.v
@@ -2,12 +2,12 @@ module LVDS(
     
 	 input CLK,
 
+	 output Channel0_P,
+	 output Channel0_N,
 	 output Channel1_P,
 	 output Channel1_N,
 	 output Channel2_P,
 	 output Channel2_N,
-	 output Channel3_P,
-	 output Channel3_N,
 	 output CLK_P,
 	 output CLK_N,
 	 output reg LED
