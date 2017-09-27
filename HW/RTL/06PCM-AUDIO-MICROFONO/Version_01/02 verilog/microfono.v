250dd0c480b0ab0c7ffaaafe9b54d4e87360ea9c
Lisseth Tatiana Herrera  Rosero <ltherreraro@unal.edu.co>
2017-09-27 09:22:49 -0500

    Update microfono.v

--- HW/RTL/06PCM-AUDIO-MICROFONO/Version_01/02 verilog/microfono.v
+++ HW/RTL/06PCM-AUDIO-MICROFONO/Version_01/02 verilog/microfono.v
@@ -1,13 +1,41 @@
 module microfono
 (
 	input     Enable,
-	input    [31:0]  data,
-	output     Clk,
+	input 	  clk,
+	output    [35:0]  Sdata,
+	output     mclk,
 	output     ws,
-	output     Dataout,
+	input     Dataint,
 	output     Done,
-	parameter   count  = 0,
+	input reset
 
 );
+
+reg count=0;
+reg [17:0] sregt
+
+div_freq df(.clk(clk), .reset(reset),clkout(mclk));
+
+always @(negedge  mclk)
+begin
+	if (Enable=1) 
+	
+		if (count<=18)
+		
+		sregt= {sregt[34:0],Dataint};
+		count ++;
+
+		else 
+			if(count=32 and Dataout)
+
+			else 
+			ws=ws!;
+			done=1;
+	
+
+	else 
+	
+end
+
 
 endmodule
