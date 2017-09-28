435fbf0978f1ad7c87e110f9b0d11824844148c3
Daniel Alejandro Rodríguez Chávez <31452436+Dantiteis@users.noreply.github.com>
2017-09-27 21:19:09 -0500

    Update contador.v

--- HW/RTL/08ULTRASONIDO/Version_02/02 verilog/contador.v
+++ HW/RTL/08ULTRASONIDO/Version_02/02 verilog/contador.v
@@ -1,13 +1,49 @@
 module contador		(
 				
-				output	[7:0] count,
-				output	trigg,
-				output	calculate,
+				output wire	[7:0] count,
+				output wire	trigg,
+				output wire	calculate,
 				input	ECHO,
 				input	ENABLE,
-				input	CLKOUT
-				
+				input	CLKOUT,
+				input	reset
 			);
-
-
+	reg [7:0] count0;
+	reg pulso
+	initial 
+	begin
+		count0=0;
+		pulso=1;
+		calculate=0;
+	end
+	
+	always@( posedge CLKOUT )
+	begin
+		if(reset)
+		begin
+			pulso=1;
+			calculate=0;
+		end
+		if(ENABLE && pulso)
+		begin
+			trigg<=1;
+			if(trigg)
+			begin
+				pulso=0;
+			end
+		end
+		else
+		begin
+			trigg=0;
+			if(ECHO)
+			begin
+				count0=count0+1;
+			end
+			else
+			begin
+				assign count = count0;
+				calculate = 1;
+			end	
+		end
+	end
 endmodule
