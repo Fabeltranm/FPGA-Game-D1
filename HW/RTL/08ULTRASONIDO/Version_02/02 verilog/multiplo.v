7d105ed14a638873e4de2d7d64a3f67fbdc8d86b
Daniel Alejandro Rodríguez Chávez <31452436+Dantiteis@users.noreply.github.com>
2017-09-27 21:19:34 -0500

    Update multiplo.v

--- HW/RTL/08ULTRASONIDO/Version_02/02 verilog/multiplo.v
+++ HW/RTL/08ULTRASONIDO/Version_02/02 verilog/multiplo.v
@@ -1,7 +1,31 @@
 module	multiplo	(
-				input   [7:0]   count,
-				input		calculate,
+				input wire  [7:0]   count,
+				input wire		calculate,
+				input wire		reset,
 				output		DONE,
-				output	[7:0]	d
+				output		[7:0]	d
 			);
+	reg [7:0] registro;
+	initial
+	begin
+		registro=0;
+		d=0;
+		DONE=0;
+	end
+	always@(calculate || reset)
+	begin
+		if(reset)
+		begin
+			registro=0;
+			d=0;
+			DONE=0;
+		end
+		else
+		begin
+			assign registro=count;
+			assign d = registro[7:0] >> 1;
+			DONE = 1;
+		end
+	end
+
 endmodule
