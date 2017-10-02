42af267323278e2eb4cffc0fc77df462b608defd
AlexandraRV <31471883+AlexandraRV@users.noreply.github.com>
2017-10-01 22:15:12 -0500

    Update registro de desplzamiento.v

--- HW/RTL/06PCM-AUDIO-MICROFONO/Version_02/02 verilog/registro de desplzamiento.v
+++ HW/RTL/06PCM-AUDIO-MICROFONO/Version_02/02 verilog/registro de desplzamiento.v
@@ -1,41 +1,24 @@
-module Registro_desplazamiento (RESET,LOAD,D_IN,D_OUT);
+module RegDes (RESET,BCLK,D_IN,DATA);
 
-parameter N  = 17;
+	// ENTRADAS Y SALIDAS DEL SISTEMA
 
-// ENTRADAS Y SALIDAS DEL SISTEMA
+	input wire RESET;
+	input wire BCLK;
+	input wire D_IN;
+	output reg [17:0] DATA;
 
-input wire RESET;
-input wire LOAD;
-input wire D_IN;
-reg [N:0]ALMACENAR;
-integer COUNT;
+	//-- Registro
 
-output reg [N:0] D_OUT;
-
-//-- Registro
-
-always @(posedge LOAD or RESET)
-begin
-  if (RESET)
-begin
-    D_IN=0;
-    D_OUT=0;
-    COUNT=0;
-end	
-  else
-  begin
-    if(COUNT==18)
-    begin
-      COUNT=1;
-      D_OUT=ALMACENAR;
-      ALMACENAR=0;
-      ALMACENAR[COUNT-1]=D_IN;
-    end
-    else
-    begin 
-      COUNT=COUNT+1;
-      ALMACENAR[COUNT-1] = D_IN;
-    end
-  end
-end
+	always @(posedge BCLK)
+		begin
+		  if (RESET)
+			begin
+				DATA=0;
+			end	
+		  else
+			begin
+				DATA = DATA << 1;
+				DATA[0] = D_IN;
+			end
+		end
 endmodule
