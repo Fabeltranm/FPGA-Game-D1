2679fda5c639d76c2695d93c7eb76555b422db1b
Lisseth Tatiana Herrera <ltherreraro@unal.edu.co>
2017-10-03 22:57:10 -0500

    prueba mic

--- HW/RTL/06PCM-AUDIO-MICROFONO/Version_01/02 verilog/prueb_mic.v
+++ HW/RTL/06PCM-AUDIO-MICROFONO/Version_01/02 verilog/prueb_mic.v
@@ -1,34 +1,46 @@
 module prueb_mic
 (
-	input     enable,
-	input 	  clk,
-	output    [31:0]  sdata,
-	output     mclk,
-	output     ws,
-	input     dataint,
-	input reset,
-	output    done
+	input		enable,
+	input		clk,
+	output reg 	[17:0]  sdata,
+	output		mclk,
+	output		ws,
+	input		dataint,
+	input		reset,
+	output		done
 
 );
 
-reg [17:0] count=18;
+reg [0:31] count=0;
 reg [17:0] sregt;
 reg done=0;
+reg ws=0;
+
 
 
 
 always @(negedge  clk, posedge enable)
 
 	if (enable) 
-		if (count<=18)
-		begin
-		sregt<= {sregt[16:0],dataint};
-		count<=count-1;
-		end		
-		else if(dataint<=0)
-		done<=1;
-	else 
-	count<=1;
+	begin	
+		if (count<=17)
+			begin
+			sregt<= {sregt[17:0],dataint};
+			count<=count+1;
+			end		
+			else if(dataint<=1'b0 && count<=30)
+			begin		
+			sdata<= sregt[17:0];
+			count<=count+1;
+			end
+			else 
+			begin
+			count<=0;				
+			ws=!ws;	
+			end
+	end	
+	else
+	done=1;
 
 
 endmodule
--- HW/RTL/06PCM-AUDIO-MICROFONO/Version_01/02 verilog/prueb_mic_TB.v
+++ HW/RTL/06PCM-AUDIO-MICROFONO/Version_01/02 verilog/prueb_mic_TB.v
@@ -1,12 +1,12 @@
 module prueb_mic_TB;
 
-reg [35:0] sdata;
 reg dataint;
 reg clk;
 reg enable;
 reg mclk;
 reg ws;
 reg done;
+reg reset;
 prueb_mic  uut(.enable(enable),.reset(reset),.dataint(dataint),.clk(clk));
 
 always
@@ -17,33 +17,57 @@ clk=1'b0;
 #2;
 end
 
-initial begin
-enable=1;
+always
+begin
+reset =1'b1;
+#130;
+reset =1'b0;
+#130;
+end
 
-#1 dataint = 1'b0;
-#2 dataint = 1'b0;
-#3 dataint = 1'b1;
+initial begin
+enable=0;
+#2 enable=1;
+end 
+initial begin
+dataint = 1'b0;
 #4 dataint = 1'b1;
-#5 dataint = 1'b0;
-#6 dataint = 1'b0;
-#7 dataint = 1'b1;
-#8 dataint = 1'b0;
-#9 dataint = 1'b0;
-#10 dataint = 1'b0;
-#11 dataint = 1'b0;
-#12 dataint = 1'b1;
-#13 dataint = 1'b1;
-#14 dataint = 1'b0;
-#15 dataint = 1'b0;
-#16 dataint = 1'b1;
-#17 dataint = 1'b0;
-#18 dataint = 1'b0;
+#4 dataint = 1'b0;
+#4 dataint = 1'b1;
+#4 dataint = 1'b0;
+#4 dataint = 1'b0;
+#4 dataint = 1'b1;
+#4 dataint = 1'b0;
+#4 dataint = 1'b1;
+#4 dataint = 1'b0;
+#4 dataint = 1'b1;
+#4 dataint = 1'b0;
+#4 dataint = 1'b1;
+#4 dataint = 1'b1;
+#4 dataint = 1'b0;
+#4 dataint = 1'b0;
+#70 dataint = 1'b1;
+#4 dataint = 1'b1;
+#4 dataint = 1'b1;
+#4 dataint = 1'b0;
+#4 dataint = 1'b1;
+#4 dataint = 1'b1;
+#4 dataint = 1'b1;
+#4 dataint = 1'b0;
+#4 dataint = 1'b1;
+#4 dataint = 1'b0;
+#4 dataint = 1'b1;
+#4 dataint = 1'b0;
+#4 dataint = 1'b1;
+#4 dataint = 1'b1;
+#4 dataint = 1'b0;
+#4 dataint = 1'b0;
 end
 	
 initial begin: TEST_CASE
      $dumpfile("prueb_mic_TB.vcd");
      $dumpvars(-1, uut);
-     #(200) $finish;
+     #(300) $finish;
    end
 
 endmodule //
