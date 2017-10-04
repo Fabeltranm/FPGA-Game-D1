decd6167e198450622fc08e47c36cb61b14d37d9
Lisseth Tatiana Herrera <ltherreraro@unal.edu.co>
2017-10-04 00:39:28 -0500

    Prueb

--- HW/RTL/06PCM-AUDIO-MICROFONO/Version_01/02 verilog/prueb_mic.v
+++ HW/RTL/06PCM-AUDIO-MICROFONO/Version_01/02 verilog/prueb_mic.v
@@ -11,36 +11,49 @@ module prueb_mic
 
 );
 
-reg [0:31] count=0;
+reg [0:31] count;
 reg [17:0] sregt;
 reg done=0;
-reg ws=0;
+reg ws=1;
 
 
 
 
 always @(negedge  clk, posedge enable)
 
-	if (enable) 
+	if (enable && done<=0) 
 	begin	
+		count<=0;
 		if (count<=17)
 			begin
-			sregt<= {sregt[17:0],dataint};
-			count<=count+1;
-			end		
-			else if(dataint<=1'b0 && count<=30)
-			begin		
-			sdata<= sregt[17:0];
+			sregt<= {sregt[16:0],dataint};
 			count<=count+1;
 			end
-			else 
+		
+		else if(dataint<=1'b0)
 			begin
-			count<=0;				
-			ws=!ws;	
-			end
+				if(count<=31)
+				begin		
+				sdata<= sregt[17:0];
+				count<=count+1;
+				end
+				else
+				begin
+				ws=!ws;
+				count<=0;
+				end								
+			end	
+		else
+			begin
+			sdata<=0;
+			done=1;
+				
+		end	
+						
 	end	
 	else
-	done=1;
+	begin
+	end
 
 
 endmodule
