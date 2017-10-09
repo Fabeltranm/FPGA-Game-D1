aa4230b559edcdfefa55ea1913b21bbc545e6396
Christian Cuestas <31107409+ChrisCuestas@users.noreply.github.com>
2017-10-09 14:51:39 -0500

    Update divisorfrec.v

--- HW/RTL/08ULTRASONIDO/Version_02/02 verilog/divisorfrec.v
+++ HW/RTL/08ULTRASONIDO/Version_02/02 verilog/divisorfrec.v
@@ -1,22 +1,28 @@
 module	divisorfrec	(
 				input           clk,
+				input		ENABLE,
 				output          CLKOUT
 			);
-	reg [11:0] count_2924;
+	reg [10:0] count_2924;
+	reg [10:0] top;
 	
 	initial
 	begin
 		CLKOUT=1'b1;
 		count_2924=0;
+		top=11'b11000010001;// top=100MHz/(2*32.2kHz)=1553, error: 0.0132%
 	end
 	
 	always @(posedge clk) 
 	begin
-		count_2924 <= count_2924 + 1;
-		if(count_2924 == 2924)
+		if(ENABLE)
 		begin
-			count_2924<=0;
-			CLKOUT <= ~CLKOUT;
+			count_2924 <= count_2924 + 1;
+			if(count_2924 == top)
+			begin
+				count_2924<=0;
+				CLKOUT <= ~CLKOUT;
+			end
 		end
 	end
 endmodule
