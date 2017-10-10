8dd5d53538f5f1f7c708a6be6bf06caa1a66ecd7
Daniel Alejandro Rodríguez Chávez <31452436+Dantiteis@users.noreply.github.com>
2017-10-10 13:41:19 -0500

    Update divisorfrecgen.v

--- HW/RTL/08ULTRASONIDO/Version_02/02 verilog/divisorfrecgen.v
+++ HW/RTL/08ULTRASONIDO/Version_02/02 verilog/divisorfrecgen.v
@@ -1,16 +1,17 @@
-module	divisorfrec	(
+module	divisorfrec	#(
+		parameter top=10'b1111101000;// top=100MHz/100kHz=1000
+			 );
+			(
 				input           clk,
 				input		ENABLE
 				output          CLKOUT1
 			);
 	reg [9:0] count_1000;
-	reg [9:0] top;
 	//
 	initial
 	begin
 		CLKOUT1=1'b0;
 		count_1000=0;
-		top=10'b1111101000;// top=100MHz/100kHz=1000
 	end
 	//
 	always @(posedge clk) 
