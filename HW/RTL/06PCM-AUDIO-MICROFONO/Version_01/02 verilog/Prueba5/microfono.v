82650d2b7c018ed5c0cff97637d9f7f73a691f2c
Lisseth Tatiana Herrera <ltherreraro@unal.edu.co>
2017-11-24 10:46:34 -0500

    Prueba4

--- HW/RTL/06PCM-AUDIO-MICROFONO/Version_01/02 verilog/Prueba5/microfono.v
+++ HW/RTL/06PCM-AUDIO-MICROFONO/Version_01/02 verilog/Prueba5/microfono.v
@@ -17,23 +17,23 @@ module microfono
 
 );
 
-wire [7:0] dout;
-wire [7:0] dout1;
+wire [320:0] dout;
+wire [320:0] dout1;
 wire mclk1;
 wire mclk2;
 assign mclk3=mclk2;
 assign mclk2=mclk1;
 assign mclk=mclk1;
-reg [7:0] sregt1;
+reg [320:0] sregt1;
 
-reg [7:0] sregt;
+reg [320:0] sregt;
 
 fifo fi(.reset(reset),.din(sregt1),.dout(dout),.clock(done),.rd(rd),.wr(wr),.empty(empty),.full(full));
 pwm pw(.ampSD(ampSD), .reset(reset),.mclk(mclk2),.ampPWM(ampPWM),.clk(clk),.dout(dout1));
 div_freq df(.clk(clk), .reset(reset),.clkout(mclk1),.led(ledres));
 
 
-reg [7:0] count;
+reg [320:0] count;
 
 
 assign dout1=dout;
@@ -52,9 +52,9 @@ begin
     		end 
 	else 
 		begin
-		if(count<=7)
+		if(count<=320)
 			begin
-			sregt<= {sregt[7:0],micData};	
+			sregt<= {sregt[320:0],micData};	
 			count<=count+1;	
 			done<=0;
 			end
