c00cc0f5ec07fe4bf02fb522c1ea96bd3025fbbb
mcamilabril <mtabrilb@unal.edu.co>
2017-11-24 19:17:54 -0500

    prueba

---
+++ HW/RTL/01BLUETOOTH/Version_02/02 verilog/Prueba1/bluetooth.v
@@ -0,0 +1,21 @@
+module bluetooth(input rx,
+                 output avail, 
+                 input clk_in,
+                 input reset,
+                 output [7:0] dout,
+                 output clk_div,
+                 input enable,
+                 output busy,
+                 output done,
+                 output tx);
+
+Divisor_Frecuencia div(.clk_in(clk_in), .clk_div(clk_div), .reset(reset));
+
+recepcion rcp(.rx(rx),.avail(avail),.dout(dout),.clk_div(clk_div));
+
+transmision trs(.enable(enable),.dout(dout),.busy(busy),.done(done),.clk_div(clk_div),.tx(tx));
+
+
+endmodule
+
+
