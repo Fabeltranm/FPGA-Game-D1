6cc9f8abbdbc230f16e08e76ab670e1a4580f8c8
Christian Cuestas <31107409+ChrisCuestas@users.noreply.github.com>
2017-11-29 09:58:26 -0500

    Add files via upload

--- HW/RTL/07TOUCH/Version_02/02 verilog/touch.v
+++ HW/RTL/07TOUCH/Version_02/02 verilog/touch.v
@@ -1,53 +1,55 @@
 module	touch	(	
-		input		ENABLE,
-		input		clk,
-          	input		Rx,
-		input		reset,
-		output 	[7:0]	data,
-		output 		DONE,
-		output 		CLKOUT,
+		input				ENABLE,
+		input				clk,
+		input				Rx,
+		input				reset,
+		output	[7:0]	data,
+		output 			DONE,
+		output 			CLKOUT
 		);
 
-		wire [7:0] data_in;
-		wire 	   wr;
-		wire 	   ps2_error;
-		wire	   read;
-		wire	   write;
-		wire	   ps2_done;
+		wire [7:0] 	data_in;
+		wire 	   	wr;
+		wire 	   	ps2_error;
+		wire	   	read;
+		wire	   	write;
+		wire	   	ps2_done;
+		wire			fifo_empty;
+		wire			fifo_full;
 
 divisorfrec	divisorfrec0	(
 					.clk		(	clk		),
-					.CLKOUT		(	CLKOUT		)
+					.CLKOUT	(	CLKOUT	)
 				);
 
 ps2		ps2_0		(
-					.Rx		(	Rx		),
+					.Rx		(	Rx				),
 					.clk		(	CLKOUT		),
-					.Rx_error	(	ps2_error	),
+					.Rx_error(	ps2_error	),
 					.DATA		(	data_in		),
-					.DONE		(	ps2_done	)
+					.DONE		(	ps2_done		)
 				);
 
 fifo		fifo0		(
-					.clk		(	CLKOUT		)
-					.reset		(	reset		),
-					.rd		(	read		),
-					.wr		(	write		),
+					.clk		(	CLKOUT		),
+					.reset	(	reset			),
+					.rd		(	read			),
+					.wr		(	write			),
 					.data_in	(	data_in		),
-					.data_out	(	data		),
+					.data_out(	data			),
 					.full		(	fifo_full	),
-					.empty		(	fifo_ empty	)
+					.empty	(	fifo_empty	)
 				);
 
 controller	controller0	(
 					.ENABLE		(	ENABLE		),
-					.reset		(	reset		),
+					.reset		(	reset			),
 					.fifo_Empty	(	fifo_empty	),
 					.fifo_full	(	fifo_full	),
-					.ps2_done	(	ps2_done	),
+					.ps2_done	(	ps2_done		),
 					.ps2_error	(	ps2_error	),
-					.DONE		(	DONE		),
-					.read		(	read		),
+					.DONE			(	DONE		),
+					.read			(	read		),
 					.write		(	write		)
 				);
 endmodule
